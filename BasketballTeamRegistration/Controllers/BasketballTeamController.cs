3b6addb87404c3d6bb1f616ebb2e35a17be7f115
Kevin Stone <ruca6616@gmail.com>
2019-07-16 16:54:20 -0700

    Added controller with single Register action method

---
+++ BasketballTeamRegistration/Controllers/BasketballTeamController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasketballTeamRegistration.Controllers
+{
+    public class BasketballTeamController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
