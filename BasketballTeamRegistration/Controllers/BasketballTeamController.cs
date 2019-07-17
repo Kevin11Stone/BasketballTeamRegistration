ec8ca350cd1c8d28e3a4df6636b74da8dc6137b4
Kevin Stone <ruca6616@gmail.com>
2019-07-16 18:25:29 -0700

    Created Team class with properties and modified BasketballTeamController.

--- BasketballTeamRegistration/Controllers/BasketballTeamController.cs
+++ BasketballTeamRegistration/Controllers/BasketballTeamController.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasketballTeamRegistration.Controllers
 {
     public class BasketballTeamController : Controller
     {
+        [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Register(IFormCollection team)
+        {
+            string teamName = team["team-name"];
             return View();
         }
     }
---
+++ BasketballTeamRegistration/Models/Team.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketballTeamRegistration.Models
+{
+    /// <summary>
+    /// Represents an indiviual team object
+    /// </summary>
+    public class Team
+    {
+        /// <summary>
+        /// Name of the team (mascot)
+        /// </summary>
+        public string TeamName { get; set; }
+
+        /// <summary>
+        /// The designated captain/leader of the team
+        /// </summary>
+        public string TeamCaptain { get; set; }
+
+        /// <summary>
+        /// The head coach of the team
+        /// </summary>
+        public string CoachName { get; set; }
+
+        /// <summary>
+        /// The team's main style of play
+        /// ex. Big, small, etc...
+        /// </summary>
+        public string Scheme { get; set; }
+
+        /// <summary>
+        /// The player that the opposing team prepares for the most
+        /// ex. PG, SG, SF, PF, C
+        /// </summary>
+        public string Playmaker { get; set; }
+
+
+    }
+}
